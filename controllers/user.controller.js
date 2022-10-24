5fb1d752901163ca31b5beb3eb338b78a2c91c65
hasibulislam999 <hasib143sl@gmail.com>
2022-10-24 14:40:55 +0600

    user controller modified

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -58,8 +58,43 @@ exports.getMe = async (req, res, next) => {
 
     res.status(200).json({
       acknowledgement: true,
-      message: "User retained",
-      description: "User logged in already",
+      message: "OK",
+      description: "User persist successfully",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* update specific user */
+exports.updateSpecificUser = async (req, res, next) => {
+  try {
+    const result = await userService.updateSpecificUser(
+      req.params.id,
+      req.body
+    );
+
+    res.status(202).json({
+      acknowledgement: true,
+      message: "Accepted",
+      description: "Successfully updated user info",
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* remove specific user */
+exports.removeSpecificUser = async (req, res, next) => {
+  try {
+    const result = await userService.removeSpecificUser(req.params.id);
+
+    res.status(202).json({
+      acknowledgement: true,
+      message: "Accepted",
+      description: "Successfully remove specified user",
       data: result,
     });
   } catch (error) {
