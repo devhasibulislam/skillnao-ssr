b0bae8d3f709822f09cc430ff2be9c37ff63bbd6
hasibulislam999 <hasib143sl@gmail.com>
2022-10-24 09:41:55 +0600

    user route modified

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,13 +3,20 @@ const express = require("express");
 
 /* internal import */
 const userController = require("../controllers/user.controller");
+const authorizedRoleMiddleware = require("../middlewares/authorizedRole.middleware");
+const verifyTokenMiddleware = require("../middlewares/verifyToken.middleware");
 
 /* router level connection */
 const router = express.Router();
 
-router.get("/me", userController.getMe);
-router.get("/all", userController.displayAllUsers);
+router.get(
+  "/all",
+  verifyTokenMiddleware,
+  authorizedRoleMiddleware("admin"),
+  userController.displayAllUsers
+);
 router.post("/signup", userController.signUpNewUser);
 router.post("/signin", userController.signInExistingUser);
+router.get("/me", verifyTokenMiddleware, userController.getMe);
 
 module.exports = router;
