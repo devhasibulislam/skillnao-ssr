bb1729a93173db38d71002e2f64d06b9c8b7f787
hasibulislam999 <hasib143sl@gmail.com>
2022-10-24 16:57:56 +0600

    course thumbnail middleware initialized

---
+++ middlewares/courseThumbnail.middleware.js
@@ -0,0 +1,34 @@
+/**
+ * Title: Upload product thumbnail
+ * Description: Create a middleware based on thumbnail upload for each product
+ * Author: Hasibul Islam
+ * Date: 24/10/2022
+ */
+
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+  destination: "thumbnails/",
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "_" + Math.round(Math.random() * 1e9);
+    cb(null, uniqueSuffix + "-" + file.originalname);
+  },
+});
+
+module.exports = multer({
+  storage,
+  fileFilter: (req, file, cb) => {
+    const supportedImage = /png|jpeg|jpg|webp/i;
+    const extension = path.extname(file.originalname);
+
+    if (supportedImage.test(extension)) {
+      cb(null, true);
+    } else {
+      cb(new Error("Must be a png/jpg/jpeg/webp formate"));
+    }
+  },
+  limits: {
+    fileSize: 1000000, // >= 1mb
+  },
+});
