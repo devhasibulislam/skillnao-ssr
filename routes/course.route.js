2be303ead8cc3cff59b78de704659b5b7f0b0c53
hasibulislam999 <hasib143sl@gmail.com>
2022-10-25 10:24:02 +0600

    course route modified

--- routes/course.route.js
+++ routes/course.route.js
@@ -3,7 +3,9 @@ const express = require("express");
 
 /* internal import */
 const courseController = require("../controllers/course.controller");
+const authorizedRoleMiddleware = require("../middlewares/authorizedRole.middleware");
 const courseThumbnailMiddleware = require("../middlewares/courseThumbnail.middleware");
+const verifyTokenMiddleware = require("../middlewares/verifyToken.middleware");
 
 /* router level connection */
 const router = express.Router();
@@ -17,10 +19,22 @@ router.post(
 router
   .route("/")
   .get(courseController.displayAllCourses)
-  .post(courseController.insertNewCourse);
+  .post(
+    verifyTokenMiddleware,
+    authorizedRoleMiddleware("admin"),
+    courseController.insertNewCourse
+  );
 router
   .route("/:id")
-  .get(courseController.displaySpecificCourse)
-  .delete(courseController.removeSpecificCourse);
+  .get(
+    verifyTokenMiddleware,
+    authorizedRoleMiddleware("admin", "user"),
+    courseController.displaySpecificCourse
+  )
+  .delete(
+    verifyTokenMiddleware,
+    authorizedRoleMiddleware("admin"),
+    courseController.removeSpecificCourse
+  );
 
 module.exports = router;
