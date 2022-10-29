4c8f9c87e0f127d7c8429b628b0c12ebe945de36
hasibulislam999 <hasib143sl@gmail.com>
2022-10-29 23:35:08 +0600

    cors origin updated

--- app.js
+++ app.js
@@ -31,7 +31,14 @@ app.use(
     },
   })
 );
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
